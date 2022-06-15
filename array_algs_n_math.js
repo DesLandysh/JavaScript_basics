8b7a5f0022eb6c2020b2d483f905b3b19d805615
Des Kitten <60898828+DesLandysh@users.noreply.github.com>
2022-06-15 17:26:11 +0300

    some instructions

--- array_algs_n_math.js
+++ array_algs_n_math.js
@@ -1,10 +1,15 @@
+// WARNING: comment min/max and code below if you want to increase the range of generater
+
+// const numbers = Array(100000).fill().map(() => Math.floor(Math.random() * 100));
 // Array Generator for 20 random numbers from 1 to 100
-const numbers = Array(20).fill().map(() => Math.floor(Math.random() * 100));
+const numbers = Array(20).fill().map(() => Math.floor(Math.random() * 100)); // comment this to use line 6 and follow Warning instructions.
+// const numbers = Array(100000).fill().map(() => Math.floor(Math.random() * 100));
 const numbers2 = [
   2, 79, 42, 70, 15, 25, 52,
   47, 94, 92, 70, 11, 34, 17,
   33, 17, 15, 18, 17, 86
 ]
+
 const odd = [];
 const even = [];
 console.time("For_of_cont")
@@ -43,7 +48,6 @@ console.timeEnd("For_loop_else")
 
 const odd4 = [];
 const even4 = [];
-
 console.time("Basic For-Loop")
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] % 2 === 0) {
@@ -60,7 +64,9 @@ console.timeEnd("Basic For-Loop")
 // BASIC FOR LOOP with ELSE-STATEMENT is FASTER!!! almost in 1.5-2 times
 
 // Check equality
+console.log("Odd equality");
 console.log(odd.length === odd2.length, odd.length === odd3.length, odd.length === odd4.length);
+console.log("Even equality");
 console.log(even.length === even2.length, even.length === even3.length, even.length === even4.length);
 
 // AVG
